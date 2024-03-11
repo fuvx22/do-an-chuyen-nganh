af51b7eae395b4f59a9b8e56d644b10f287e8b54
fuvx22 <voxngocphu@gmail.com>
2024-03-11 10:05:07 +0700

    fix missing folder

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -9,7 +9,7 @@ const userRouter =  express.Router();
 userRouter.route('/')
   .get( async (req, res) => {
     try {
-
+    
       const newUser = await createNew(req.body);
       res.status(200).json(newUser)
 
@@ -34,8 +34,12 @@ userRouter.route('/login')
 
     const { userId, password } = req.body;
 
+    console.log(userId)
+    console.log(password)
+
     // Tìm kiếm user trong database
     const user = users.find(u => u.userId === userId);
+
 
     // Kiểm tra mật khẩu
     if (!user || user.password != password) {
