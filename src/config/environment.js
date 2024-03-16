731dd2fe5fd1bd0c3a49075d93d19b651a8230ff
Hoang Phuc <hoangphuc1996vnvn@gmail.com>
2024-03-16 10:35:49 +0700

    phuc login

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,10 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const env = {
   DATABASE_URI: process.env.DB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  PORT: process.env.PORT
-}
+  PORT: process.env.PORT,
+  SECRET_KEY: process.env.SECRET_KEY,
+};
 
-module.exports = env
+module.exports = env;
