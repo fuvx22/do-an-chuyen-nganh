fe3845e442cb19fb33139b3b773bee52309fecd8
fuvx22 <voxngocphu@gmail.com>
2024-04-17 22:54:00 +0700

    course registration API

---
+++ src/controller/courseRegisController.js
@@ -0,0 +1,40 @@
+const courseRegisModel = require('../model/courseRegisModel');
+const { StatusCodes } = require("http-status-codes");
+
+const createNewCourseRegis = async (req, res) => {
+  try {
+    const newCourseRegis = req.body;
+    const createdCourseRegis = await courseRegisModel.createNewCourseRegis(newCourseRegis);
+    res.status(StatusCodes.CREATED).json(createdCourseRegis);
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+  }
+};
+
+const getCourseRegisByUserId = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const courseRegis = await courseRegisModel.getCourseRegisByUserId(userId);
+    res.status(StatusCodes.OK).json(courseRegis);
+  } catch (error) {
+    res.status(StatusCodes.NOT_FOUND).json({ error: error.message });
+  }
+};
+
+const deleteCourseRegis = async (req, res) => {
+  try {
+    const courseRegisToDelete = req.body;
+    const result = await courseRegisModel.deleteCourseRegis(courseRegisToDelete);
+    res.status(StatusCodes.OK).json(result);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
+  }
+};
+
+const courseRegisController = {
+  createNewCourseRegis,
+  getCourseRegisByUserId,
+  deleteCourseRegis,
+};
+
+module.exports = courseRegisController;
